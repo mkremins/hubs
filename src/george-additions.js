d459a45eae8bea7cb1d78f90b7f7468c8e123be2
George Butler <georgebutleriv@gmail.com>
2021-04-30 15:54:24 -0700

    Debug messages

--- src/george-additions.js
+++ src/george-additions.js
@@ -35,6 +35,7 @@ function bargeTick() {
     const bargeMaxZ = currentPos.z + BARGE_Z_SIZE / 2;
 
     // check if local user's avatar inside barge X/Z column; move it along with the barge if so
+    // eslint-disable-next-line no-undef
     const avatar = APP.componentRegistry["player-info"][0].el;
     const pos = avatar.getAttribute("position");
     if (pos.x >= bargeMinX && pos.x <= bargeMaxX && pos.z >= bargeMinZ && pos.z <= bargeMaxZ) {
@@ -62,11 +63,14 @@ function spawnBarge() {
     setInterval(bargeTick, BARGE_TICKRATE);
     // eslint-disable-next-line no-undef
     APP.scene.appendChild(barge);
+  } else {
+    console.log("Cannot Create: Barge already exists.");
   }
 }
 
 function startBarge() {
   if (!barge) {
+    console.warn("Cannot Start: Barge is non-existant.");
     return;
   }
 
@@ -75,6 +79,7 @@ function startBarge() {
 
 function stopBarge() {
   if (!barge) {
+    console.warn("Cannot Stop: Barge is non-existant.");
     return;
   }
 
